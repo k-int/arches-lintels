91b2129c55b7a1e8d98218a9af4b102a57f8997d
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-03-09 08:50:55 +0000

    updates and add qss

--- lintels.py
+++ lintels.py
@@ -6,6 +6,8 @@ from arches_lintels.controllers.mainwindow import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+    with open("arches_lintels/resources/lintels.qss", "r") as f:
+        app.setStyleSheet(f.read())    
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec())
