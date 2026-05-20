50431b88e4208eb45cc982849b97b8bdf6f887be
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-05-20 08:54:21 +0100

    Black format main window

--- arches_lintels/controllers/mainwindow.py
+++ arches_lintels/controllers/mainwindow.py
@@ -1,6 +1,6 @@
 import os
 
-from PyQt6.QtGui import QIcon, QCursor, QPixmap, QTransform
+from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QDir, QSize, QProcess
 from PyQt6.QtWidgets import QMainWindow
 
@@ -9,6 +9,8 @@ from arches_lintels.views.ui_mainwindow import Ui_MainWindow
 from arches_lintels.controllers.settings_page import SettingsPage
 
 from arches_lintels.models.dependencies.postgres import PostgresModel
+
+
 class MainWindow(QMainWindow):
     """
     Lintels Main Window interface class
@@ -31,33 +33,43 @@ class MainWindow(QMainWindow):
     def init_ui(self):
         QDir.addSearchPath("img", os.path.join(APP_ROOT, "img"))
 
-        self.ui.homeButtonIconOnly.setIcon(QIcon("img:icons/fa-archway-solid-white.svg"))
-        self.ui.homeButtonIconOnly.setIconSize(QSize(20,20))
+        self.ui.homeButtonIconOnly.setIcon(
+            QIcon("img:icons/fa-archway-solid-white.svg")
+        )
+        self.ui.homeButtonIconOnly.setIconSize(QSize(20, 20))
         self.ui.homeFull.setIcon(QIcon("img:icons/fa-archway-solid-white.svg"))
-        self.ui.homeFull.setIconSize(QSize(20,20))
+        self.ui.homeFull.setIconSize(QSize(20, 20))
 
-        self.ui.controlCentreIconOnly.setIcon(QIcon("img:icons/fa-bars-progress-solid-white.svg"))
-        self.ui.controlCentreIconOnly.setIconSize(QSize(20,20))
-        self.ui.controlCentreFull.setIcon(QIcon("img:icons/fa-bars-progress-solid-white.svg"))
-        self.ui.controlCentreFull.setIconSize(QSize(20,20))
+        self.ui.controlCentreIconOnly.setIcon(
+            QIcon("img:icons/fa-bars-progress-solid-white.svg")
+        )
+        self.ui.controlCentreIconOnly.setIconSize(QSize(20, 20))
+        self.ui.controlCentreFull.setIcon(
+            QIcon("img:icons/fa-bars-progress-solid-white.svg")
+        )
+        self.ui.controlCentreFull.setIconSize(QSize(20, 20))
 
         self.ui.archesCentreIconOnly.setIcon(QIcon("img:Arches_icon_white.svg"))
-        self.ui.archesCentreIconOnly.setIconSize(QSize(20,20))
+        self.ui.archesCentreIconOnly.setIconSize(QSize(20, 20))
         self.ui.archesCentreFull.setIcon(QIcon("img:Arches_icon_white.svg"))
-        self.ui.archesCentreFull.setIconSize(QSize(20,20))
+        self.ui.archesCentreFull.setIconSize(QSize(20, 20))
 
         self.ui.settingsIconOnly.setIcon(QIcon("img:icons/fa-gear-solid-white.svg"))
-        self.ui.settingsIconOnly.setIconSize(QSize(20,20))
+        self.ui.settingsIconOnly.setIconSize(QSize(20, 20))
         self.ui.settingsFull.setIcon(QIcon("img:icons/fa-gear-solid-white.svg"))
-        self.ui.settingsFull.setIconSize(QSize(20,20))
+        self.ui.settingsFull.setIconSize(QSize(20, 20))
 
-        self.ui.aboutIconOnly.setIcon(QIcon("img:icons/fa-circle-question-regular-white.svg"))
-        self.ui.aboutIconOnly.setIconSize(QSize(20,20))
-        self.ui.aboutFull.setIcon(QIcon("img:icons/fa-circle-question-regular-white.svg"))
-        self.ui.aboutFull.setIconSize(QSize(20,20))
+        self.ui.aboutIconOnly.setIcon(
+            QIcon("img:icons/fa-circle-question-regular-white.svg")
+        )
+        self.ui.aboutIconOnly.setIconSize(QSize(20, 20))
+        self.ui.aboutFull.setIcon(
+            QIcon("img:icons/fa-circle-question-regular-white.svg")
+        )
+        self.ui.aboutFull.setIconSize(QSize(20, 20))
 
         self.ui.menuButton.setIcon(QIcon("img:icons/fa-bars-solid.svg"))
-        self.ui.menuButton.setIconSize(QSize(20,20))
+        self.ui.menuButton.setIconSize(QSize(20, 20))
 
         self.ui.versionIconOnly.setText(VERSION)
         self.ui.versionFull.setText(f"Lintels {VERSION}")
@@ -70,20 +82,40 @@ class MainWindow(QMainWindow):
         # Needs to change index for both full and collapsed navbar
         self.ui.stackedWidget.setCurrentIndex(0)
 
-        self.ui.homeButtonIconOnly.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(0))
-        self.ui.homeFull.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(0))
-        
-        self.ui.controlCentreIconOnly.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(1))
-        self.ui.controlCentreFull.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(1))
-        
-        self.ui.archesCentreIconOnly.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(2))
-        self.ui.archesCentreFull.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(2))
-        
-        self.ui.settingsIconOnly.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(3))
-        self.ui.settingsFull.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(3))
+        self.ui.homeButtonIconOnly.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(0)
+        )
+        self.ui.homeFull.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(0)
+        )
 
-        self.ui.aboutIconOnly.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(4))
-        self.ui.aboutFull.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(4))
+        self.ui.controlCentreIconOnly.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(1)
+        )
+        self.ui.controlCentreFull.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(1)
+        )
+
+        self.ui.archesCentreIconOnly.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(2)
+        )
+        self.ui.archesCentreFull.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(2)
+        )
+
+        self.ui.settingsIconOnly.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(3)
+        )
+        self.ui.settingsFull.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(3)
+        )
+
+        self.ui.aboutIconOnly.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(4)
+        )
+        self.ui.aboutFull.clicked.connect(
+            lambda: self.ui.stackedWidget.setCurrentIndex(4)
+        )
 
     def navbar_menu_btn_change(self):
         if self.ui.menuButton.isChecked():
@@ -98,4 +130,6 @@ class MainWindow(QMainWindow):
 
         self.init_process = QProcess(self)
         self.init_process.start(initdb_exe, args)
-        self.init_process.finished.connect(self.postgres_model.on_init_postgres_finished)
+        self.init_process.finished.connect(
+            self.postgres_model.on_init_postgres_finished
+        )
