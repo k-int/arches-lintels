9a89554d44a2833a277021526482727bf3ef2ee3
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-05-20 16:00:04 +0100

    Move util

--- arches_lintels/controllers/control_centre_page.py
+++ arches_lintels/controllers/control_centre_page.py
@@ -2,11 +2,11 @@ from PyQt6.QtCore import QProcess
 
 from functools import partial
 
-from arches_lintels.utils.update_widget_styling import update_widget_styling
 
 from arches_lintels.models.settings_model import SettingsModel
 from arches_lintels.models.dependencies.postgres import PostgresModel
 from arches_lintels.controllers.components.file_browser import file_browser
+from arches_lintels.controllers.utils.update_widget_styling import update_widget_styling
 
 
 class ControlCentreController():
