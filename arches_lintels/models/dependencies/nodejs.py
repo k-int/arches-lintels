ef55f05ccf3a63b942e77f0d4bc7e6293728ff09
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-06-03 17:38:29 +0100

    Set git path in npm QProcess environment

--- arches_lintels/models/dependencies/nodejs.py
+++ arches_lintels/models/dependencies/nodejs.py
@@ -9,19 +9,40 @@ class NodeModel:
     def get_nodejs_path(self):
         return self.settings_model.get_config_value(["dependencies","nodejs","install_directory"])
 
+    @property
+    def get_git_path(self):
+        return self.settings_model.get_config_value(["dependencies","git","install_directory"])
+
+    def _create_npm_cache(self):
+        npm_cache_dir = os.path.join(self.get_nodejs_path, "npm-cache")
+        if not os.path.exists(npm_cache_dir):
+            os.makedirs(npm_cache_dir)
+        return npm_cache_dir
+
     def get_node_exe_path(self):
-        """
-        NodeJS doesn't have a bin/ dir unlike psql or ES, so we just return the top level containing
-        npm, npx, and node.exe.
-        """
         return os.path.join(self.get_nodejs_path, "node.exe")
+
+    def get_git_exe_path(self):
+        return os.path.join(self.get_git_path, "cmd", "git.exe")
 
     def build_node_environment(self, qprocessenv):
         """
-        Implements Node into a QProcessEnvironment
+        Implements Node & Git into a QProcessEnvironment.
+        Returns the environment for attaching to a QProcess
         """        
         # Get the existing path and append the lintels nodejs install path to it
         path = qprocessenv.value("PATH")
         new_path = f"{self.get_nodejs_path};{path}"
         qprocessenv.insert("PATH", new_path)
+
+        # Ensure node/npm doesn't log to or create files in AppData or user
+        npm_cache_dir = self._create_npm_cache()
+        qprocessenv.insert("npm_config_cache", npm_cache_dir)
+        qprocessenv.insert("npm_config_prefix", self.get_nodejs_path)
+        qprocessenv.insert("npm_config_globalconfig", os.path.join(self.get_nodejs_path, "npmrc"))
+        qprocessenv.insert("npm_config_userconfig", os.path.join(self.get_nodejs_path, ".npmrc"))
+
+        git_exe = self.get_git_exe_path()
+        qprocessenv.insert("npm_config_git", git_exe)
+
         return qprocessenv
