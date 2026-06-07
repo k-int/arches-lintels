7eb0680bc05ad3640f40a4e719cb7e2c5873ac6d
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-06-07 12:10:52 +0100

    update log to remove wrong encodings

--- arches_lintels/controllers/utils/qprocess_debugging.py
+++ arches_lintels/controllers/utils/qprocess_debugging.py
@@ -1,17 +1,22 @@
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
 
 def _formatting(message):
-    return message.rstrip("\r\n")
+    ansi_escape = re.compile(r'\x1b\[[0-9;]*[a-zA-Z]')
+    message = ansi_escape.sub('', message)
+    message = message.rstrip("\r\n")
+    print(message)
+    return message
 
 def read_stderr(qprocess):
-    logger.warning(_formatting(bytes(qprocess.readAllStandardError()).decode()))
+    logger.warning(_formatting(bytes(qprocess.readAllStandardError()).decode("utf-8")))
 
 
 def read_stdout(qprocess):
-    logger.debug(_formatting(bytes(qprocess.readAllStandardOutput()).decode()))
+    logger.debug(_formatting(bytes(qprocess.readAllStandardOutput()).decode("utf-8")))
 
 
 def handle_process_error(error):
